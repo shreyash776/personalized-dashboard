90b4b350f3b14b313ecbb6a4a286285b0eca7bd6
Shreyash chaurasia <shreyashchaurasia77@gmail.com>
2025-07-22 02:17:58 +0530

    Refactor FeedPage component: improve loading state handling and integrate movie data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
 'use client'
-import { useSelector } from 'react-redux'
 import { useGetTopHeadlinesQuery } from '../services/newsApi'
+import { useGetPopularMoviesQuery } from '../services/tmdbApi'
+import { useSelector } from 'react-redux'
 import { RootState } from '../features/store'
 import NewsCard from '../components/cards/NewsCard'
+import MovieCard from '../components/cards/MovieCard'
 
 export default function FeedPage() {
   const categories = useSelector((state: RootState) => state.user.categories)
-  const { data, error, isLoading } = useGetTopHeadlinesQuery(categories[0]) 
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>Error loading news</div>
-
+  const { data: newsData, isLoading: newsLoading } = useGetTopHeadlinesQuery(categories[0])
+  const { data: movieData, isLoading: movieLoading } = useGetPopularMoviesQuery('')
+  
+  if (newsLoading || movieLoading) return <div>Loading...</div>
+  
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {data?.articles.map((news: any) => (
+      
+      {newsData?.articles.map((news: any) => (
         <NewsCard key={news.url} article={news} />
+      ))}
+      
+      {movieData?.results.map((movie: any) => (
+        <MovieCard key={movie.id} movie={movie} />
       ))}
     </div>
   )
