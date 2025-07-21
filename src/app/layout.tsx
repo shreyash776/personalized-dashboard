d43944f5420a0075b8b951d60a0a09a943f75fbd
Shreyash chaurasia <shreyashchaurasia77@gmail.com>
2025-07-22 01:41:26 +0530

    Refactor Redux store implementation: restore ReduxProvider and reintroduce store configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
 import './globals.css'
 import Sidebar from '../components/layout/Sidebar'
 import Header from '../components/layout/Header'
-// import { ReduxProvider } from '../features/store'
+import { ReduxProvider } from '../features/store'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="flex min-h-screen bg-gray-100 dark:bg-gray-900">
-        {/* <ReduxProvider> */}
+        <ReduxProvider>
           
           <Sidebar />
           <div className="flex-1 flex flex-col">
             <Header />
             <main className="flex-1 p-4">{children}</main>
           </div>
-        {/* </ReduxProvider> */}
+        </ReduxProvider>
       </body>
     </html>
   );
--- src/features/store.tsx
+++ src/features/store.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import userReducer from './user/userSlice'
