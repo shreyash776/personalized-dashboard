e91b3fd355057e67c83b61278175fe14a83ea874
Shreyash chaurasia <shreyashchaurasia77@gmail.com>
2025-07-22 12:59:17 +0530

    Wrap layout components with ThemeBody for dark mode support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,19 @@ import './globals.css'
 import Sidebar from '../components/layout/Sidebar'
 import Header from '../components/layout/Header'
 import { ReduxProvider } from '../features/store'
-
+import ThemeBody from "../components/layout/ThemeBody";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="flex min-h-screen bg-gray-100 dark:bg-gray-900">
         <ReduxProvider>
-          
+          <ThemeBody>
           <Sidebar />
           <div className="flex-1 flex flex-col">
             <Header />
             <main className="flex-1 p-4">{children}</main>
           </div>
+         </ThemeBody>
         </ReduxProvider>
       </body>
     </html>
